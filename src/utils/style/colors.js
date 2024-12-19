7fa5b87aacf9a5d41ce3169d0a6e9f696eabe364
unknown <louisepop14@gmail.com>
2024-12-19 14:32:16 +0100

    mise à jour fichier couleur

--- src/utils/style/colors.js
+++ src/utils/style/colors.js
@@ -2,6 +2,10 @@ const colors = {
     black: '#020203',
     red: '#E60000',
     white: '#FFFFFF',
+    gray: '#FBFBFB',
+    darkGray: '#282D30',
+    lightGray: '#74798C',
+    lightLightGray: '#DEDEDE',
 }
 
 export default colors
