a894c09c7ba51390a64256c17961677793ccb69a
unknown <louisepop14@gmail.com>
2025-01-04 13:04:31 +0100

    étape 4: prénom #4

--- src/components/Dashboard/Welcome.jsx
+++ src/components/Dashboard/Welcome.jsx
@@ -1,12 +1,11 @@
-import React, { useState, useEffect } from 'react';
-import { useParams } from 'react-router-dom';
+import React, { useState, useEffect } from 'react'
+import { useParams } from 'react-router-dom'
 
-import styled from 'styled-components';
-import colors from '../../utils/style/colors';
+import styled from 'styled-components'
+import colors from '../../utils/style/colors'
 
-import { mockUserData } from '../../utils/API/mockAPI';
-//import { fetchUserData } from '../utils/API/serviceAPI';
-
+import { mockUserData } from '../../utils/API/mockAPI'
+import { fetchUserData } from '../../utils/API/serviceAPI'
 
 const WelcomeText = styled.p`
   margin-top: 18px;
@@ -14,33 +13,32 @@ const WelcomeText = styled.p`
   color: ${colors.black};
   font-size: 48px;
   font-weight: 600;
-`;
+`
 
 const WelcomeSpan = styled.span`
   color: ${colors.red};
-`;
+`
 
-function Welcome() {
-  const { userId } = useParams();
-  const [userName, setUserName] = useState('');
+function Welcome({ useMockData }) {
+  const { userId } = useParams()
+  const [userName, setUserName] = useState('')
 
   useEffect(() => {
-    //utilisation des données mockées
-    const user = mockUserData[userId];
-
-    if (user) {
-      setUserName(user.userInfos.firstName);
+    if (useMockData) {
+      //utilisation des données mockées
+      const user = mockUserData[userId];
+      if (user) {
+        setUserName(user.userInfos.firstName);
+      } else {
+        console.error('Utilisateur non trouvé');
+      }
     } else {
-      console.error('Utilisateur non trouvé');
-    }
-
-    //version finale avec les données de l'API
-    /*
-    fetchUserData(userId)
-      .then(data => setUserName(data.userInfos.firstName))
-      .catch(error => console.error('Erreur lors de la récupération des données:', error));
-    */
-  }, [userId]); //useEffect déclanché chaque fois que userId change
+      //version finale avec les données de l'API
+      fetchUserData(userId)
+        .then(data => setUserName(data.userInfos.firstName))
+        .catch(error => console.error('Erreur lors de la récupération des données:', error))
+     }
+  }, [userId, useMockData]); //useEffect déclenché chaque fois que userId ou useMockData change
 
   return (
     <WelcomeText>
